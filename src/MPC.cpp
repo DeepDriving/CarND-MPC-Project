0d12d9ec93c30fdaf0a2d71468ede3b390b745a4
DeepDriving <tawbay@gmail.com>
2017-06-21 10:37:13 -0700

    Add files via upload

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -34,7 +34,7 @@ const double Lf = 2.67;
 // The reference velocity is set to 40 mph.
 double ref_cte = 0;
 double ref_epsi = 0;
-double ref_v = 40*0.44704;
+double ref_v = 30*0.44704;
 
 // The solver takes all the state variables and actuator
 // variables in a singular vector. Thus, we should to establish
